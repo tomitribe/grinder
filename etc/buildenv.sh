5a0eb9edea9e85ac682e7d7b1448fa91d4d914a7
Philip Aston <philipa@mail.com>
2013-06-13 18:53:52 +0100

    Explicitly add mvn to the path. Remove clover as we always use it through eclipse.

--- etc/buildenv.sh
+++ etc/buildenv.sh
@@ -1,9 +1,7 @@
 export ANT_HOME=/opt/ant/apache-ant-1.7.1
 export JAVA_HOME=/opt/hotspot/jdk1.6.0_30/
-export PATH=${ANT_HOME}/bin:${JAVA_HOME}/bin:$PATH
+export MVN_HOME=/opt/maven/apache-maven-3.0.5
 
-# Clover home also needs to be set in localpaths.properties.
-CLOVER=/opt/clover/clover-ant-2.0.3/lib/clover.jar
-#XALAN=/opt/xalan/xalan-j_2_3_1/bin/xalan.jar:/opt/xalan/xalan-j_2_3_1/bin/xml-apis.jar
-
-export CLASSPATH="${CLOVER}"
+export PATH=${ANT_HOME}/bin:${PATH}
+export PATH=${JAVA_HOME}/bin:${PATH}
+export PATH=${MVN_HOME}/bin:${PATH}
